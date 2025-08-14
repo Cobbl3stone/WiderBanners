04528726c72c81640248fcb47e867512a0a00e46
Daniel <danigonlo2@gmail.com>
2025-08-14 18:46:22 +0200

    Initial commit

---
+++ src/main/java/com/example/WiderBannersClient.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WiderBannersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
